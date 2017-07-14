0f920d9eca02cec575239e6d45346694e3ff006c
Maxime Rossier <rossier.maxime@gmail.com>
2017-07-14 19:27:38 +0200

    create a basic vuetify layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,74 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+  <v-app dark toolbar footer>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-tile
+          router
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed>
+      <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn
+        icon
+        @click.native.stop="miniVariant = !miniVariant"
+      >
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.native.stop="clipped = !clipped"
+      >
+        <v-icon>web</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.native.stop="fixed = !fixed"
+      >
+        <v-icon>remove</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </main>
+    <v-footer :fixed="fixed">
+      <span>footer</span>
+    </v-footer>
+  </v-app>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        miniVariant: false,
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
